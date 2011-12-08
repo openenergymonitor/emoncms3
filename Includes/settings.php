f2a5185cc6810609c09c6d40a4ce7dc8a3448b2f
Trystan Lea <trystan.lea@googlemail.com>
2011-12-08 00:23:49 +0000

    Update Includes/settings.php

--- Includes/settings.php
+++ Includes/settings.php
@@ -1,7 +1,7 @@
 <?php
 
-  $username = "root";
-  $password = "xczxxe4001";
-  $server   = "localhost";
-  $database = "emonphp";
+  $username = "";
+  $password = "";
+  $server   = "";
+  $database = "";
 ?>
