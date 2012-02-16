10b7c4abc4fe538ee99532ac36d9414709cdf5e0
Trystan <trystan@trystan-laptop.(none)>
2012-02-16 15:39:33 +0000

    Fixed multigraph path errors

--- Vis/MultiGraph/multigraph.php
+++ Vis/MultiGraph/multigraph.php
@@ -13,9 +13,9 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <script language="javascript" type="text/javascript" src="<?php echo $path; ?>flot/jquery.js"></script>
-    <script language="javascript" type="text/javascript" src="<?php echo $path; ?>flot/jquery.flot.js"></script>
-    <script language="javascript" type="text/javascript" src="<?php echo $path; ?>flot/jquery.flot.selection.js"></script>
+    <script language="javascript" type="text/javascript" src="<?php echo $path; ?>Vis/flot/jquery.js"></script>
+    <script language="javascript" type="text/javascript" src="<?php echo $path; ?>Vis/flot/jquery.flot.js"></script>
+    <script language="javascript" type="text/javascript" src="<?php echo $path; ?>Vis/flot/jquery.flot.selection.js"></script>
 </head>
 <body style="font-family:arial;">
   
@@ -120,8 +120,8 @@
      var feedIn = [];
 
      $.ajax({                                    
-            url: path+'api/getfeed',                         
-            data: "&apikey="+apikey+"&feedid="+feedID+"&start="+start+"&end="+end+"&resolution="+resolution,  
+            url: path+'feed/data.json',                         
+            data: "&apikey="+apikey+"&id="+feedID+"&start="+start+"&end="+end+"&res="+resolution,  
        dataType: 'json',                           
        async: false,
        success: function(datain) { feedIn = datain;}
