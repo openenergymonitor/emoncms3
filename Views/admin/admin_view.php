f0c2fbb25f31df874d2d3e513c5fe8899e4324ce
Trystan <trystan@trystan-laptop.(none)>
2012-03-08 23:01:38 +0000

    1024 bytes in a kb of course...

--- Views/admin/admin_view.php
+++ Views/admin/admin_view.php
@@ -1,6 +1,6 @@
 <div class='lightbox' style="margin-bottom:20px; margin-left:3%; margin-right:3%;">
 <h1>Admin panel</h1>
-Total feed data disk space use: <?php echo number_format($total_memuse/1000,1); ?> KiB
+Total feed data disk space use: <?php echo number_format($total_memuse/1024,1); ?> KiB
 
 <h2>Users</h2>
 <h3><?php echo sizeof($userlist); ?> registered users</h3>
@@ -12,7 +12,7 @@ Total feed data disk space use: <?php echo number_format($total_memuse/1000,1); 
   <tr class="<?php echo 'd'.($i & 1); ?> " >
     <td><?php echo $user['userid']; ?></td>
     <td><?php echo $user['name']; ?></td>
-    <td><?php echo number_format($user['memuse']/1000,1); ?> KiB</td>
+    <td><?php echo number_format($user['memuse']/1024,1); ?> KiB</td>
     <td><?php echo $user['admin']; ?></td>
   </tr>
 <?php } ?>
