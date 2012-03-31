7fd9a3e8fa8bc602dfd1f9e3b960088aadd248c5
trystan <trystan@emonvm.default.openenergymon.uk0.bigv.io>
2012-03-31 15:24:21 +0000

    updated IE compatibility

--- Vis/realtime.php
+++ Vis/realtime.php
@@ -27,6 +27,7 @@
 
  <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <!--[if IE]><script language="javascript" type="text/javascript" src="<?php echo $path;?>Vis/flot/excanvas.min.js"></script><![endif]-->
     <script language="javascript" type="text/javascript" src="<?php echo $path;?>Vis/flot/jquery.js"></script>
     <script language="javascript" type="text/javascript" src="<?php echo $path;?>Vis/flot/jquery.flot.js"></script>
  </head>
