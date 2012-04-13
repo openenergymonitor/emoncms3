6e216ae603fb22582587c9eec518d5ee8bca60ad
trystan <trystan@emonvm.default.openenergymon.uk0.bigv.io>
2012-04-13 23:36:44 +0000

    Added histogram feed creation

--- conv.php
+++ conv.php
@@ -23,7 +23,7 @@
   $e = db_connect();
 
   require "Models/feed_model.php";
-  $feeds = get_all_feeds();
+  //$feeds = get_all_feeds();
   //$feeds = get_user_feed_ids(1);
 
   foreach ($feeds as $feed ){
@@ -37,6 +37,11 @@
       // 1) rename feed to be converted as temporary
       $result = db_query("RENAME TABLE `$feedname` TO `feed_tmp`");
 
+      $result = db_query("SELECT * FROM feed_tmp LIMIT 1");
+      $row = db_fetch_array($result);
+      if(!isset($row['data2']))
+      {
+
       // 2) Create the new feed table with index
       $result = db_query("CREATE TABLE $feedname (
       `time` INT UNSIGNED NOT NULL ,
@@ -46,10 +51,25 @@
       // 3) Copy over and convert time from datetime to unix timestamp
       $result = db_query("INSERT $feedname SELECT UNIX_TIMESTAMP (time),data FROM feed_tmp");
 
+      } else {
+
+      // 2) Create the new feed table with index
+      $result = db_query("CREATE TABLE $feedname (
+      `time` INT UNSIGNED NOT NULL ,
+      `data` FLOAT NOT NULL ,
+      `data2` FLOAT NOT NULL ,
+      INDEX ( `time` ))");
+
+      // 3) Copy over and convert time from datetime to unix timestamp
+      $result = db_query("INSERT $feedname SELECT UNIX_TIMESTAMP (time),data,data2 FROM feed_tmp");
+
+      }
+
       // 4) Drop the temporary feed table
       $result = db_query("DROP TABLE `feed_tmp`");
 
       // 5) Update feed type status
+      $feedid = $feed['id'];
       $result = db_query("UPDATE feeds SET type = 1 WHERE id='$feedid'");
 
     } else { echo "feed: ".$feed['id']." already converted<br/>";}
