98099578f84911db13fdf5bc0cfd3c826c8ec864
Trystan <trystan@trystan-laptop.(none)>
2012-04-26 11:36:30 +0100

    changed user_view to include dashboard style blocks around specific items making for a clearer layout

--- Views/user_view.php
+++ Views/user_view.php
@@ -8,9 +8,12 @@
     http://openenergymonitor.org
 -->
 
+
 <div class='lightbox' style="margin-bottom:20px; margin-left:3%; margin-right:3%;">
 
   <h2>User: <?php echo $user['username']; ?></h2>
+
+  <div class="widget-container-nc" style="width:600px;">
 
   <h3>API keys</h3>
   <table>
@@ -33,13 +36,19 @@
     </tr>
   </table>
 
+  </div>
+  <div style="clear:both;"></div><br/>
+
   <?php
   $testjson = $GLOBALS['path']."api/post?apikey=".$user['apikey_write']."&json={power:252.4,temperature:15.4}"
   ?>
-
+  <div class="widget-container-nc"  style="width:600px;">
   <p><b>API url: </b><?php echo $GLOBALS['path']; ?>api/post</p>
   <p><b>Example: Copy this to your web browser or send from a nanode: </b><br/><?php echo $testjson; ?> <a href="<?php echo $testjson; ?>">try me</a></p>
+  </div>
+  <div style="clear:both;"></div><br/>
 
+<div class="widget-container-nc"  style="width:600px;">
 <h3>Change password</h3>
 <form action="changepass" method="get">
 <p><b>Old password:</b><br/>
@@ -48,8 +57,10 @@
 <input class="inp01" type="password" name="newpass" style="width:250px"/></p>
 <input type="submit" class="button04" value="Change" /> 
 </form>
+</div>
+  <div style="clear:both;"></div><br/>
 
-<br/>
+<div class="widget-container-nc"  style="width:600px;">
 <h3>Account Statistics</h3>
 
 <table>
@@ -57,7 +68,7 @@
   <tr><td>Up hits:</td><td><?php echo $stats['uphits']; ?></td></tr>
   <tr><td>Down hits:</td><td><?php echo $stats['dnhits']; ?></td></tr>
 </table>
-
+</div>
 </div>
 
 
