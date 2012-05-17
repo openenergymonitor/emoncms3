3f07a3272236fcc036cf3cd74f02b23d42f3a869
Ildefonso Martinez <i.martinezmarchena@gmail.com>
2012-05-17 18:50:53 +0200

    New add graph tool bar button.

---
+++ Includes/editors/dialogs/e3graph.js
@@ -0,0 +1,25 @@
+CKEDITOR.dialog.add( 'e3graph', function( editor ) {
+
+	return {
+		title : 'Insert dial dialog',
+		minWidth : 400,
+		minHeight : 200,
+		onOk : function() {
+			feed = this.getContentElement('tab1','feed').getValue();
+			idd = this.getContentElement('tab1','idd').getValue();
+			
+			editor.insertHtml("<div class='graph' feed='"+feed+"' id='"+idd+"'></div>");    
+		},
+		contents : [
+			{
+			id : 'tab1',
+			label : '',
+			title : '',
+			elements :[
+				{  id : 'feed', type : 'text', label : 'Feed', labelLayout: 'horizontal' },
+				{  id : 'idd', type : 'text', label : 'id', labelLayout: 'horizontal' }
+			]
+			}
+		]
+	};
+} );
