6eff91499b0b2c194da22c0fafab5683ff255eab
Trystan <trystan@trystan-laptop.(none)>
2012-07-13 14:46:18 +0100

    Improved menu

--- Views/dash/dash_view.php
+++ Views/dash/dash_view.php
@@ -10,11 +10,21 @@
 
 <?php if ($session['write'] && $_SESSION['editmode'] ==TRUE) { ?>
 <div style="width:100%; background-color:#ddd;">
-  <div style="margin: 0px auto; text-align:right; width:940px;">
-    <ul class="greydashmenu"><?php echo $menu; ?></ul>
-    <div style="padding:5px; margin-right:5px;">
-      <a href="edit?id=<?php echo $dashid; ?>">Edit</a>
-    </div>
+  <div style="margin: 0px auto; text-align:left; width:940px;">
+
+    <span style="float:left; color:#888; font: 13px/27px sans-serif; font-weight:bold; ">Dashboards:</span>
+    <ul class="greydashmenu">
+      <?php echo $menu; ?>
+      <li><a href="../dashboards/new"><b>+ New</b></a></li>
+    </ul>
+
+    <ul class="greydashmenu" style="float:right; padding-right:5px;">
+      <li><a href="edit?id=<?php echo $dashid; ?>">Draw</a></li>
+      <li><a href="../dashboard/view?id=<?php echo $dashid; ?>">CKEditor</a></li>
+      <li><a href="source?id=<?php echo $dashid; ?>">Source</a></li>
+    </ul>
+    <span style="float:right; color:#888; font: 13px/27px sans-serif; font-weight:bold; ">Edit:</span>
+    <div style="clear:both"></div>
   </div>
 </div><br>
 <?php } ?>
