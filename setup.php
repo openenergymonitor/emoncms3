c1392b4c5151f431a4f071c8f16d3f65fb279d43
trystan <trystan@emonvm.default.openenergymon.uk0.bigv.io>
2012-08-03 07:25:27 +0000

    node implementation

--- setup.php
+++ setup.php
@@ -65,6 +65,7 @@ $schema['input'] = array(
   'id' => array('type' => 'int(11)', 'Null'=>'NO', 'Key'=>'PRI', 'Extra'=>'auto_increment'),
   'userid' => array('type' => 'text'),
   'name' => array('type' => 'text'),
+  'nodeid' => array('type' => 'int(11)'),
   'processList' => array('type' => 'text'),
   'time' => array('type' => 'datetime'),
   'value' => array('type' => 'float')
