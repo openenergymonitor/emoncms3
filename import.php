a84d687d92c69695b42af2330541be290074714c
Trystan <trystan@trystan-laptop.(none)>
2012-09-25 17:49:49 +0100

    Increased import.php execution time to 2 mins

--- import.php
+++ import.php
@@ -17,6 +17,8 @@
   // IMPORTANT SET THIS TO A UNIQUE PASSWORD OF YOUR CHOICE
   if ($_GET['key'] != "xTC7005d") die;
 
+  set_time_limit (120);
+
   // Ensure only one instance of the script can run at any one time.
   $fp = fopen("importlock", "w");
   if (! flock($fp, LOCK_EX | LOCK_NB)) { echo "Already running\n"; die; }
